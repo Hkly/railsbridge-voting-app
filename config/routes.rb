ce8d4ef023f26402ae238813d99ccfd17078121d
Hannah Yiu <hannahkyiu@gmail.com>
2014-04-10 22:37:28 -0700

    CSS and design attempts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Suggestotron::Application.routes.draw do
+  get "static_pages/home"
+  get "static_pages/help"
+  get "static_pages/about"
   resources :topics
 
   # The priority is based upon order of creation: first created -> highest priority.
